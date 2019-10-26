91e15fa6a2016420a07a30c865814d64c169af6c
anitoanto <anitoperamangalam@gmail.com>
2019-10-27 00:47:42 +0530

    Dynamic space background added.

---
+++ UnityProject/StellarThrust/Assets/Scripts/Rock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        gameObject.transform.rotation = Random.rotation;
+        Destroy(gameObject,Random.Range(40,50));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(Random.Range(-9,-2),0,0);
+    }
+}
---
+++ UnityProject/StellarThrust/Assets/Scripts/SpaceMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceMovement : MonoBehaviour
+{
+    public GameObject rock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0;i < 30;i++) {
+            Instantiate(rock, new Vector3(Random.Range(-60, 80), Random.Range(-30, 60), Random.Range(0, 20)), Quaternion.identity);
+        }
+        InvokeRepeating("CreateRocks",0,4);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateRocks()
+    {
+        for(int i = 0;i < 10;i++) {
+            Instantiate(rock, new Vector3(Random.Range(70, 100), Random.Range(-30, 60), Random.Range(0, 20)), Quaternion.identity);
+        }
+    }
+}
